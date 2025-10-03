582de86da18dbb9b57362825a81b88527146713a
RedietNegash <redietnegash9@gmail.com>
2025-10-03 02:19:42 -0700

    Add: regulatory_effect/2 and eqtl_association/2 as multifile.

--- kg_rule_mining/load_kg_with_rules.pl
+++ kg_rule_mining/load_kg_with_rules.pl
@@ -1,10 +1,12 @@
-% load_kg_with_rules.pl
+% load_kg_with_rules.pl (modified)
 :- style_check(-discontiguous).
 :- multifile gene/1.
 :- multifile snp/1.
 :- multifile enhancer/1.
 :- multifile associated_with/2.
 :- multifile regulates/2.
+:- multifile regulatory_effect/2.
+:- multifile eqtl_association/2.
 
 % File paths for knowledge graph files
 user:file_search_path(gencode_gene, '/mnt/hdd_1/abdu/prolog_out_v2/gencode/gene').
@@ -13,6 +15,7 @@ user:file_search_path(dbsnp_edges, '/mnt/hdd_1/abdu/prolog_out_v2/dbsnp/edges.pl
 user:file_search_path(enhancer_nodes, '/mnt/hdd_1/abdu/prolog_out_v2/enhancer_atlas/nodes.pl').
 user:file_search_path(enhancer_edges, '/mnt/hdd_1/abdu/prolog_out_v2/enhancer_atlas/edges.pl').
 user:file_search_path(dbsuper_edges, '/mnt/hdd_1/abdu/prolog_out_v2/dbsuper/edges.pl').
+user:file_search_path(relevant_gene_kb, '/mnt/hdd_1/abdu/prolog_out_v2/metta_out_v5').  % for regulatory_effect / eqtl_association
 
 % Load a list of files safely with timing
 load_with_time(Files, Name) :-
@@ -27,20 +30,31 @@ load_minimal_kg :-
     load_with_time([enhancer_nodes, enhancer_edges], "enhancers"),
     load_with_time([dbsuper_edges], "associations").
 
+% Load relevant gene facts safely
+load_relevant_gene_facts :-
+    load_with_time([relevant_gene_kb('regulatory_effect.pl'),
+                    relevant_gene_kb('eqtl_association.pl')], "relevant_gene_facts").
+
 % Load KG + mined rules
 load_kg_with_rules :-
     format("Loading knowledge graph...~n"),
     load_minimal_kg,
+    load_relevant_gene_facts,
     format("Loading mined rules...~n"),
     consult('output/mined_rules.lpad'),
     format("Knowledge graph and rules loaded successfully!~n").
 
 % Dump all triples to TSV: predicate(subject, object)
-dump_triples :-
-    open('data/kg_triples.tsv', write, Stream),
+dump_triples(File) :-
+    open(File, write, Stream),
     forall(
         (clause(Pred(X,Y), _)),
         format(Stream, "~w\t~w\t~w~n", [Pred, X, Y])
     ),
     close(Stream),
-    format("Dumped all triples to data/kg_triples.tsv~n").
+    format("Dumped all triples to ~w~n", [File]).
+
+% Convenience: load KG and dump to a specific TSV
+load_and_dump(File) :-
+    load_kg_with_rules,
+    dump_triples(File).
