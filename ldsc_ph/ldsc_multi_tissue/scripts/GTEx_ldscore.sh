209793cb2a761b026d24de724a4afd04e0ca159a
RedietNegash <redietnegash9@gmail.com>
2026-02-03 00:24:37 -0800

    initialize batch LD score calculation script and director

---
+++ ldsc_ph/ldsc_multi_tissue/scripts/GTEx_ldscore.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+LDSC_PATH="/mnt/hdd_1/rediet/hypothesis-generation-demo/ldsc_ph/ldsc/ldsc_repo/ldsc.py"
+GENE_SETS="/mnt/hdd_1/rediet/hypothesis-generation-demo/ldsc_ph/ldsc_multi_tissue/results/gtex/tissue_gene_sets/*_top10.txt"
+BFILE_DIR="/mnt/hdd_1/rediet/hypothesis-generation-demo/ldsc_ph/ldsc/GRCh38/plink_files/plink_files"
+ANNOT_DIR="/mnt/hdd_1/rediet/hypothesis-generation-demo/ldsc_ph/ldsc_multi_tissue/results/gtex/annot_files"
+OUT_DIR="/mnt/hdd_1/rediet/hypothesis-generation-demo/ldsc_ph/ldsc_multi_tissue/results/gtex/ldscores"
+
+
+mkdir -p "$OUT_DIR"
