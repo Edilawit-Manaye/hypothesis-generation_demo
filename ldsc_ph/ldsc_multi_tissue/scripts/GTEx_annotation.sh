cbd1eee47e20c81553bb4526df06343bda0f6c6b
RedietNegash <redietnegash9@gmail.com>
2026-02-03 00:12:52 -0800

    genecode_v26_Grch38 symbols along with plinkfiles absolute files

---
+++ ldsc_ph/ldsc_multi_tissue/scripts/GTEx_annotation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+cd /mnt/hdd_1/rediet/hypothesis-generation-demo/ldsc_ph/ldsc_multi_tissue/results/gtex/
+
+
+MAKE_ANNOT_SCRIPT="/mnt/hdd_1/rediet/hypothesis-generation-demo/ldsc_ph/ldsc/ldsc_repo/make_annot.py"
+GENE_COORD_FILE="/mnt/hdd_1/rediet/hypothesis-generation-demo/ldsc_ph/ldsc_multi_tissue/data/gtex/gencode.v26.GRCh38.symbols.txt"
+BIM_BASE_PATH="/mnt/hdd_1/rediet/hypothesis-generation-demo/ldsc_ph/ldsc/GRCh38/plink_files/plink_files"
+
+
+mkdir -p annot_files
+
+
